f3d093792af36903eb4562894abcb975f72cf47b
Daniil Chemaev <chemaev.ds@gmail.com>
2023-04-19 11:27:01 +0300

    Edited relative date

--- BananaTube/Presentation/Subscriptions/VideoCollectionViewCell.swift
+++ BananaTube/Presentation/Subscriptions/VideoCollectionViewCell.swift
@@ -29,6 +29,7 @@ class VideoCollectionViewCell: UICollectionViewCell {
     let titleLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 2
+        label.font = label.font.withSize(16)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -37,6 +38,7 @@ class VideoCollectionViewCell: UICollectionViewCell {
         let label = UILabel()
         label.textColor = .gray
         label.numberOfLines = 2
+        label.font = label.font.withSize(14)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -68,9 +70,12 @@ class VideoCollectionViewCell: UICollectionViewCell {
 
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZ"
-        let date = dateFormatter.date(from: videoInfo.publishedAt)
+        let date = Date().addingTimeInterval(dateFormatter.date(from: videoInfo.publishedAt)!.timeIntervalSinceNow)
 
-        subtitleTextView.text = "\(videoInfo.channelTitle!) \(statistics.viewCount) views \(date!.timeIntervalSince1970)"
+        let formatter = RelativeDateTimeFormatter()
+        let relativeDate = formatter.localizedString(for: date, relativeTo: Date())
+
+        subtitleTextView.text = "\(videoInfo.channelTitle!) \(statistics.viewCount) views \(relativeDate)"
 
         guard let url = URL(string: videoInfo.thumbnails.high.url) else { return }
 
@@ -83,16 +88,16 @@ class VideoCollectionViewCell: UICollectionViewCell {
         NSLayoutConstraint.activate([
             thumbnailImageView.topAnchor.constraint(equalTo: topAnchor, constant: 5),
             thumbnailImageView.heightAnchor.constraint(equalToConstant: 215),
-            thumbnailImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
-            thumbnailImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+            thumbnailImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            thumbnailImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
 
             userProfileImageView.topAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: 10),
-            userProfileImageView.leadingAnchor.constraint(equalTo: thumbnailImageView.leadingAnchor),
-            userProfileImageView.heightAnchor.constraint(equalToConstant: 50),
-            userProfileImageView.widthAnchor.constraint(equalToConstant: 50),
+            userProfileImageView.leadingAnchor.constraint(equalTo: thumbnailImageView.leadingAnchor, constant: 5),
+            userProfileImageView.heightAnchor.constraint(equalToConstant: 36),
+            userProfileImageView.widthAnchor.constraint(equalToConstant: 36),
 
             titleLabel.topAnchor.constraint(equalTo: userProfileImageView.topAnchor),
-            titleLabel.leftAnchor.constraint(equalTo: userProfileImageView.rightAnchor, constant: 5),
+            titleLabel.leftAnchor.constraint(equalTo: userProfileImageView.rightAnchor, constant: 12),
             titleLabel.trailingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor),
 
             subtitleTextView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
